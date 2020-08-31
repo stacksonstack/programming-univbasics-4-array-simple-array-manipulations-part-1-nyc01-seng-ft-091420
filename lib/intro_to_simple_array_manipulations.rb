bb6f7f5136da90a4bb6d79c2fcd247f82e2a9f35
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-08-31 23:13:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,12 @@ end
 bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
 new_neighborhood = "Staten Island"
 updated_array = using_unshift(bouroughs_in_nyc, new_neighborhood)
+
+def using_pop(array)
+  array.pop
+end
+
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+deleted_string = using_pop(continents)
+
+
