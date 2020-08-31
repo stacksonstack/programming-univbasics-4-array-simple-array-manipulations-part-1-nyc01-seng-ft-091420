96c3869d409d7306847c063482006bc3ee4cd1a7
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-08-31 23:15:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,3 +22,20 @@ continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oce
 deleted_string = using_pop(continents)
 
 
+def pop_with_args(array)
+  array.pop(2)
+end
+
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+small_dogs = pop_with_args(dog_breeds)
+  end 
+  it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
+    expect(@small_dogs).to eq(["Chihuahua", "Shiba Inu"])
+  end 
+
+  it "decreases the length of the array by 2" do
+    expect(@dog_breeds.size).to eq(2)
+  end
+end
+
+
